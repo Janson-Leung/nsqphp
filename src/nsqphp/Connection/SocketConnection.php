e13c55ee8a45d2c776e3738759876591ce6bb2ea
liangjianshan <liangjs@ecqun.com>
2017-06-05 14:01:38 +0800

    Fixed nonblock socket_select

--- src/nsqphp/Connection/SocketConnection.php
+++ src/nsqphp/Connection/SocketConnection.php
@@ -62,16 +62,16 @@ class SocketConnection implements ConnectionInterface {
     /**
      * Optional on-connect callback
      * 
-     * @var callable|NULL
+     * @var callable|null
      */
     private $connectCallback;
 
     /**
      * Socket handle
      * 
-     * @var Resource|NULL
+     * @var Resource|null
      */
-    private $socket = NULL;
+    private $socket = null;
     
     /**
      * Constructor
@@ -85,18 +85,18 @@ class SocketConnection implements ConnectionInterface {
      *      available before giving up (eg; duirng SUB loop)
      *      In seconds (no need to be whole numbers)
      * @param boolean $nonBlocking Put socket in non-blocking mode
-     * @param callable|NULL $connectCallback Optional on-connect callback (will
+     * @param callable|null $connectCallback Optional on-connect callback (will
      *      be called whenever we establish a connection)
      */
     public function __construct(
             $hostname = 'localhost',
-            $port = NULL,
+            $port = null,
             $connectionTimeout = 3,
             $readWriteTimeout = 3,
             $readWaitTimeout = 15,
-            $nonBlocking = FALSE,
-            $connectCallback = NULL
-            ) {
+            $nonBlocking = false,
+            $connectCallback = null
+        ) {
         $this->hostname = $hostname;
         $this->port = $port ? $port : 4150;
         $this->connectionTimeout = $connectionTimeout;
@@ -124,7 +124,8 @@ class SocketConnection implements ConnectionInterface {
     public function isReadable() {
         $read = array($socket = $this->getSocket());
         $readable = @socket_select($read, $null, $null, $this->readWaitTimeoutSec, $this->readWaitTimeoutUsec);
-        return $readable ? TRUE : FALSE;
+
+        return $readable ? true : false;
     }
     
     /**
@@ -135,19 +136,111 @@ class SocketConnection implements ConnectionInterface {
      * @return string Binary data
     */
     public function read($length) {
-        $read = 0;
-        $parts = [];
+        $null = null;
+        $socket_read = array($socket = $this->getSocket());
 
-        while ($read < $length) {
-            $data = @socket_read($this->getSocket(), $length - $read, PHP_BINARY_READ);
-            if ($data === false) {
-                $this->error('Failed to read data from ' . $this->__toString());
+        $surplus = $length;
+        $data = '';
+        while ($surplus > 0) {
+            $readable = socket_select($socket_read, $null, $null, $this->readWriteTimeout['sec'], $this->readWriteTimeout['usec']);
+
+            if ($readable > 0) {
+                $fread = @socket_read($socket, $surplus, PHP_BINARY_READ);
+
+                if ($fread === false) {
+                    $this->error('Failed to read data from ' . $this->getDomain());
+                } elseif ($fread == '') {
+                    $this->error('NsqSocket read 0 bytes from ' . $this->getDomain(), SOCKET_ECONNRESET);
+                }
+            } elseif ($readable === 0) {
+                throw new SocketException(
+                    sprintf('Timed out reading %d bytes from %s after %d seconds and %d microseconds', $surplus,
+                        $this->getDomain(), $this->readWriteTimeout['sec'], $this->readWriteTimeout['usec'])
+                );
+            } else {
+                throw new SocketException(
+                    sprintf('Could not read %d bytes from %s', $surplus, $this->getDomain())
+                );
             }
-            $read += strlen($data);
-            $parts[] = $data;
+
+            $data .= $fread;
+            $surplus -= strlen($fread);
         }
 
-        return implode($parts);
+        return $data;
+    }
+
+    /**
+     * Write to the socket.
+     *
+     * @param string $data The data to write
+     */
+    public function write($data) {
+        $null = null;
+        $write = array($socket = $this->getSocket());
+
+        $surplus = strlen($data);
+        while($surplus > 0) {
+            $writable = socket_select($null, $write, $null, $this->readWriteTimeout['sec'], $this->readWriteTimeout['usec']);
+
+            if ($writable > 0) {
+                $written = @socket_write($this->getSocket(), $data);
+
+                if ($written === false) {
+                    $this->error('Failed to write to ' . $this->getDomain());
+                }
+            } elseif ($writable === 0) {
+                throw new SocketException(
+                    sprintf('Timed out writing %d bytes to %s after %d seconds and %d microseconds', $surplus,
+                        $this->getDomain(), $this->readWriteTimeout['sec'], $this->readWriteTimeout['usec'])
+                );
+            } else {
+                throw new SocketException(
+                    sprintf('Could not write %d bytes to %s', $surplus, $this->getDomain())
+                );
+            }
+
+            $data = substr($data, $written);
+            $surplus -= $written;
+        }
+    }
+
+    /**
+     * Get socket handle
+     * 
+     * @return Resource The socket
+     */
+    public function getSocket() {
+        if ($this->socket === null) {
+            $this->socket = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+            if ($this->socket === false) {
+                throw new SocketException('Failed to create socket');
+            }
+
+            if (@socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, $this->readWriteTimeout) === false) {
+                $this->error('Failed to set socket recv timeout option');
+            }
+
+            if (@socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, $this->readWriteTimeout) === false) {
+                $this->error('Failed to set socket send timeout option');
+            }
+
+            if (@socket_connect($this->socket, $this->hostname, $this->port) === false) {
+                throw new ConnectionException('Failed to connect socket to ' . $this->getDomain());
+            }
+
+            if ($this->nonBlocking) {
+                socket_set_nonblock($this->socket);
+            }
+
+            // on-connection callback
+            if ($this->connectCallback !== null) {
+                call_user_func($this->connectCallback, $this);
+            }
+        }
+
+        return $this->socket;
     }
 
     /**
@@ -160,85 +253,41 @@ class SocketConnection implements ConnectionInterface {
             @socket_shutdown($this->socket);
             @socket_close($this->socket);
         }
-        $this->socket = NULL;
+
+        $this->socket = null;
         return $this->getSocket();
     }
 
     /**
-     * Write to the socket.
+     * Get the domain that this socket is connected to
      *
-     * @param string $data The data to write
+     * @return string
      */
-    public function write($data) {
-        $written = 0;
-        $length = strlen($data);
+    public function getDomain() {
+        return "{$this->hostname}:{$this->port}";
+    }
 
-        while($written < $length) {
-            $fwrite = @socket_write($this->getSocket(), substr($data, $written));
-            if ($fwrite === false) {
-                $this->error('Failed to write buffer to ' . $this->__toString());
-            }
-
-            $written += $fwrite;
-        }
+    /**
+     * To string (for debug logging)
+     *
+     * @return string
+     */
+    public function __toString() {
+        return $this->getDomain();
     }
 
     /**
      * Fail with socket error
      *
      * @param string $msg
+     * @param int $errno
      *
      * @throws SocketException
      */
-    private function error($msg) {
-        $errmsg = @socket_strerror($errno = socket_last_error($this->socket));
+    private function error($msg, $errno = 0) {
+        $errno = $errno ?: socket_last_error($this->socket);
+        $errmsg = @socket_strerror($errno);
+
         throw new SocketException("{$errmsg} -> {$msg}", $errno);
-    }
-
-    /**
-     * Get socket handle
-     * 
-     * @return Resource The socket
-     */
-    public function getSocket() {
-        if ($this->socket === NULL) {
-            $this->socket = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-
-            if ($this->socket === false) {
-                throw new SocketException('Failed to create socket to ' . $this->__toString());
-            }
-
-            if (@socket_connect($this->socket, $this->hostname, $this->port) === false) {
-                throw new SocketException('Failed to connect socket to ' . $this->__toString());
-            }
-
-            if (@socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, $this->readWriteTimeout) === false) {
-                $this->error('Failed to set socket stream recv timeout option on ' . $this->__toString());
-            }
-
-            if (@socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, $this->readWriteTimeout) === false) {
-                $this->error('Failed to set socket stream send timeout option on ' . $this->__toString());
-            }
-
-            if ($this->nonBlocking) {
-                socket_set_nonblock($this->socket);
-            }
-
-            // on-connection callback
-            if ($this->connectCallback !== NULL) {
-                call_user_func($this->connectCallback, $this);
-            }
-        }
-
-        return $this->socket;
-    }
-    
-    /**
-     * To string (for debug logging)
-     * 
-     * @return string
-     */
-    public function __toString() {
-        return "{$this->hostname}:{$this->port}";
     }
 }
